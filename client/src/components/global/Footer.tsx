23be22317173265827a3390c362a535acbb9715a
Nikita Babko <niki.babko@gmail.com>
2021-07-21 18:02:10 +0700

    Added components: Header and Footer .

---
+++ client/src/components/global/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="text-center bg-light py-4">
+      <h6>Welcome to my GitHub</h6>
+      <a
+        href="https://github.com/nikitababko"
+        target="_blank"
+        rel="noreferrer"
+        className="mb-2 d-block"
+      >
+        https://github.com/nikitababko
+      </a>
+      <p> Copyright &copy; 2021</p>
+    </div>
+  );
+};
+
+export default Footer;
