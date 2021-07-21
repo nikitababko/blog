478bc07d4cc1559e3966fc9d3b8e87cf8bd621f5
Nikita Babko <niki.babko@gmail.com>
2021-07-21 20:01:41 +0700

    Added login page.

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import LoginPass from 'components/auth/LoginPass';
+import test from 'styles/Button.module.scss';
 
 const Login = () => {
-  return (
-    <div>
-      <h2>Login Page</h2>
-    </div>
-  )
-}
+  const [sms, setSms] = useState(false);
 
-export default Login
+  return (
+    <div className="auth_page">
+      <div className="auth_box">
+        <h3 className="text-uppercase text-center mb-4">Login</h3>
+
+        <LoginPass />
+
+        <small
+          className="row my-2 text-primary"
+          style={{ cursor: 'pointer' }}
+        >
+          <span className="col-6">
+            <Link to="/forgot_password">Forgot password?</Link>
+          </span>
+
+          <span className="col-6 text-end" onClick={() => setSms(!sms)}>
+            {sms ? 'Sign in with password' : 'Sign in with SMS'}
+          </span>
+        </small>
+
+        <p>
+          You don't have an account?
+          <Link to={`/register`} style={{ color: 'crimson' }}>
+            {` Register Now`}
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
