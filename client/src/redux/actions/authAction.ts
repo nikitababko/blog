b9da12e809e4a3dc5fc3457c7dbdce42ec130ddd
Nikita Babko <niki.babko@gmail.com>
2021-07-22 20:23:22 +0700

    Added alert component: loading, error, success.

--- client/src/redux/actions/authAction.ts
+++ client/src/redux/actions/authAction.ts
@@ -1,12 +1,19 @@
 import { Dispatch } from 'redux';
-
 import { AUTH, IAuthType } from 'redux/types/authType';
+import { ALERT, IAlertType } from 'redux/types/alertType';
+
 import { IUserLogin } from 'utils/TypeScript';
 import { postAPI } from 'utils/FetchData';
 
 export const login =
-  (userLogin: IUserLogin) => async (dispatch: Dispatch<IAuthType>) => {
+  (userLogin: IUserLogin) =>
+  async (dispatch: Dispatch<IAuthType | IAlertType>) => {
     try {
+      dispatch({
+        type: ALERT,
+        payload: { loading: true },
+      });
+
       const res = await postAPI('login', userLogin);
 
       dispatch({
@@ -16,7 +23,15 @@ export const login =
           user: res.data.user,
         },
       });
+
+      dispatch({
+        type: ALERT,
+        payload: { success: 'Login Success!' },
+      });
     } catch (error: any) {
-      console.log(error.response.data.message);
+      dispatch({
+        type: ALERT,
+        payload: { errors: error.response.data.message },
+      });
     }
   };
