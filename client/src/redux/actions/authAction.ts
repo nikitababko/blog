d9d34adfb78946c9adbda65a6d9a7c6e53d57831
Nikita Babko <niki.babko@gmail.com>
2021-07-23 13:08:44 +0700

    Added ability: register.

--- client/src/redux/actions/authAction.ts
+++ client/src/redux/actions/authAction.ts
@@ -2,8 +2,9 @@ import { Dispatch } from 'redux';
 import { AUTH, IAuthType } from 'redux/types/authType';
 import { ALERT, IAlertType } from 'redux/types/alertType';
 
-import { IUserLogin } from 'utils/TypeScript';
+import { IUserLogin, IUserRegister } from 'utils/TypeScript';
 import { postAPI } from 'utils/FetchData';
+import { validRegister } from 'utils/Valid';
 
 export const login =
   (userLogin: IUserLogin) =>
@@ -32,6 +33,40 @@ export const login =
       dispatch({
         type: ALERT,
         payload: { errors: error.response.data.message },
+      });
+    }
+  };
+
+export const register =
+  (userRegister: IUserRegister) =>
+  async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+    const check = validRegister(userRegister);
+
+    if (check.errorLength > 0) {
+      return dispatch({
+        type: ALERT,
+        payload: { errors: check.errorMessage },
+      });
+    }
+
+    try {
+      dispatch({
+        type: ALERT,
+        payload: { loading: true },
+      });
+
+      const res = await postAPI('register', userRegister);
+
+      dispatch({
+        type: ALERT,
+        payload: { success: res.data.message },
+      });
+    } catch (error: any) {
+      dispatch({
+        type: ALERT,
+        payload: {
+          errors: error.response.data.message,
+        },
       });
     }
   };
--- client/src/utils/TypeScript.ts
+++ client/src/utils/TypeScript.ts
@@ -1,5 +1,4 @@
 import { ChangeEvent, FormEvent } from 'react';
-
 import rootReducer from '../redux/reducers/index';
 
 export type InputChange = ChangeEvent<HTMLInputElement>;
@@ -16,6 +15,11 @@ export interface IParams {
 export interface IUserLogin {
   account: string;
   password: string;
+}
+
+export interface IUserRegister extends IUserLogin {
+  name: string;
+  cf_password: string;
 }
 
 export interface IUser extends IUserLogin {
